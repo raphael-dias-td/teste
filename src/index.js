f66cd6d59d99beefd5af9120ecc682ff2538f158
raphael <raphael@tentaculo.digital>
2020-08-28 13:39:56 -0300

    global style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import GlobalStyle from "./global";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
